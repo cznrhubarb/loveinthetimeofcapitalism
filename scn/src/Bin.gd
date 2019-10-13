9bfcc72c8b1a0d30a0911481d7a2b30717b5478f
Will McAuliff <womcauliff@gmail.com>
2019-10-13 09:28:26 -0400

    creates Bin scene.

--- scn/src/Bin.gd
+++ scn/src/Bin.gd
@@ -1,15 +1,17 @@
 extends Sprite
 
-# TODO set a local bin var for item_type, set by constructor
-var item_type = "NOTEBOOK"
-
-# TODO add constructor
+# TODO set a local aisle id var
+var aisle_id = "A"
+var item_type = ""
 
 onready var tween = get_tree().get_root().find_node("HUDTween", true, false)
 onready var menu = get_tree().get_root().find_node("BinContentsMenu", true, false)
+# TODO find itemplacer node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# TODO call itemplacer method to retrieve bin contents
+	# item_type = itemplacer.place_random_item(aisle_id)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,9 +19,15 @@ func _ready():
 #	pass
 
 
+# TODO implement _input listener to add bin contents to inventory
+# func _input(event):
+#    if event.is_action_pressed("pickup_item"):
+#       inventory.add_item(item_type)
+
+
 func _on_Area2D_body_entered(body):
 	# Ignore the Wall's collision
-	if body == get_parent().get_node("StaticBody2D"):
+	if body == get_parent().get_node("Wall/StaticBody2D"):
 		return
 	tween.interpolate_property(menu, "position", Vector2(530, -400), Vector2(530, 200), .5, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	tween.start()
